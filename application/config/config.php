ca03f94a314278d2f1094f19abe4b7845bac656f
Krishna Uppili <krishnauppili@Krishnas-MacBook-Pro.local>
2017-11-29 16:24:49 +0530

    deploy test7

--- application/config/config.php
+++ application/config/config.php
@@ -52,7 +52,8 @@ $config['index_page'] = '';
 |
 | WARNING: If you set this to 'PATH_INFO', URIs will always be URL-decoded!
 */
-$config['uri_protocol']	= 'REQUEST_URI';
+$_SERVER['PATH_INFO'] = $_SERVER['ORIG_PATH_INFO'];
+$config['uri_protocol']	= 'PATH_INFO';
 
 /*
 |--------------------------------------------------------------------------
