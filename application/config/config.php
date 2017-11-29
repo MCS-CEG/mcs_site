f750e415a1a013395c21a101feffb8ad5ee0b1d6
Krishna Uppili <krishnauppili@Krishnas-MacBook-Pro.local>
2017-11-29 15:43:54 +0530

    deploy test2

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/mcs/';
+$config['base_url'] = 'http://floating-caverns-83920.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
